e39ddddda0a4814d1315ead22337b631ce563b0f
Ege Erciyas <egeerciyas@Eges-MacBook-Pro.local>
2025-02-17 11:41:30 +0300

    medusa store , e-zane oldu

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            E-zanee deneme 1.0 Hosgeldiniz !
+            EGE SIKER  DENEME 1
           </Heading>
           <Heading
             level="h2"
@@ -23,9 +23,7 @@ const Hero = () => {
           href="https://funkyton.com/medusajs-2-0-is-finally-here/"
           target="_blank"
         >
-          <h1 style={{ textDecoration: "underline" }}>
-            Visit the tutorial
-          </h1>
+
         </a>
       </div>
     </div>
