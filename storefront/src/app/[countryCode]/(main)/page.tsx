cac72b40883d04dc772b537a5081565858a737a5
erciyas99 <erciyasege@gmail.com>
2025-02-19 17:06:20 +0300

    Description change

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -6,9 +6,9 @@ import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "e-zane",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "Türkiye'nin e-zanesi",
 }
 
 export default async function Home({
